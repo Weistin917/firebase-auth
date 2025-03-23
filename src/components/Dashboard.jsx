34a1ab08bee2c6de333a98963190cad48367df31
Wei-Chung Cheng <weichung.cheng@galileo.edu>
2025-03-23 17:35:59 -0600

    4th commit. Dashboard completed.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,13 +2,26 @@ import React from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebaseConfig";
 import { useNavigate } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 function Dashboard() {
+    const navigate = useNavigate();
+    const user = auth.currentUser;
+
+    const handleLogout = async () => {
+        try {
+            await signOut(auth);
+            navigate("/");
+        } catch (err) {
+            console.error("Error logging out: ", err);
+        }
+    };
+
     return (
         <div className="d-grid gap-3">
             <h4>Dashboard</h4>
-            <h5>Welcome</h5>
-            <Button variant="danger" type="submit" size="lg">Log out</Button>
+            <h5>Welcome, {user?.email}!</h5>
+            <Button variant="danger" onClick={handleLogout} size="lg">Log out</Button>
         </div>
     );
 }
